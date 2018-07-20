9cb9de496eaa77228238983894a1a64d8a42dbe8
Sijan Shreesh Rana <Sijan_Rana@hotmail.com>
2018-07-20 16:53:09 +0100

    established link between vertx and unity

--- ElectricBoxMaintenance/Assets/FuseBoxStateManager.cs
+++ ElectricBoxMaintenance/Assets/FuseBoxStateManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using VertexUnityPlayer;
 
 [RequireComponent(typeof(SceneLink))]
-public class FuseBoxStateManager : MonoBehaviour {
+public class FuseBoxStateManager : MonoBehaviour
+{
 
     public SceneLink sceneLink;
 
@@ -12,29 +13,20 @@ public class FuseBoxStateManager : MonoBehaviour {
     public GameObject handKeyAnimation;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         sceneLink = GetComponent<SceneLink>();
         sceneLink.OnStateChange += SceneLink_OnStateChange;
-	}
+    }
 
     private void SceneLink_OnStateChange(SceneLinkStatus oldState, SceneLinkStatus newState)
     {
-        if(newState == SceneLinkStatus.Connected)
+        if (newState == SceneLinkStatus.Connected)
         {
             Debug.Log("VERTX connected");
             StartCoroutine(SetupFusebox());
         }
 
-        if(newState == SceneLinkStatus.Disconnected)
-        {
-            Debug.Log("VERTX disconnected");
-            //finds and destorys gameobjects created
-            DestroyObject(sceneLink.transform.Find("fusebox-vertx").gameObject);
-            DestroyObject(sceneLink.transform.Find("hand-Key-Animation").gameObject);
-            DestroyObject(sceneLink.transform.Find("arrow-switch-1").gameObject);
-            DestroyObject(sceneLink.transform.Find("arrow-switch-2").gameObject);
-            DestroyObject(sceneLink.transform.Find("arrow-switch-3").gameObject);
-        }
     }
 
     IEnumerator SetupFusebox()
@@ -42,57 +34,74 @@ public class FuseBoxStateManager : MonoBehaviour {
         yield return new WaitForSeconds(2.0f);
 
         //ElectricBox
-        var box = sceneLink.transform.Find("fusebox-vertx");
-        if (box == null)
-            FuseboxNode = SceneLink.Instance.CreateNode(
-                "fusebox-vertx",
-                new Vector3(0, 0, 0),
-                Quaternion.identity,
-                Vector3.one,
-                "863b5c9a-7b83-4a88-b4d6-41a33bdba80e"
-                );
-        else
-            FuseboxNode = box.gameObject;
-
-            handKeyAnimation = SceneLink.Instance.CreateNode(
-            "hand-Key-Animation",
-            new Vector3(0f, 0f, 0f),
-            Quaternion.identity,
-            Vector3.one,
-            "ac100581-430a-4817-b2c8-9978144b521f"
-            );
-
-        GameObject switchOne = SceneLink.Instance.CreateNode(
-            "arrow-switch-1",
-            new Vector3(0f, 0f, 0f),
-            Quaternion.identity,
-            Vector3.one,
-            "36748b64-8251-4b76-8672-d67d5522dfb1"
-            );
-
-        GameObject switchTwo = SceneLink.Instance.CreateNode(
-            "arrow-switch-2",
-            new Vector3(0f, 0f, 0f),
-            Quaternion.identity,
-            Vector3.one,
-            "c74cf1eb-da54-448e-b5c2-a423d16064a4"
-            );
-
-        GameObject switchThree = SceneLink.Instance.CreateNode(
-            "arrow-switch-3",
-            new Vector3(0f, 0f, 0f),
-            Quaternion.identity,
-            Vector3.one,
-            "ec53c75e-ec10-4014-8d96-f73f2e27ca82"
-            );
+        //var box = sceneLink.transform.Find("fusebox-vertx");
+        //if (box == null)
+        //    FuseboxNode = SceneLink.Instance.CreateNode(
+        //        "fusebox-vertx",
+        //        new Vector3(0, 0, 0),
+        //        Quaternion.identity,
+        //        Vector3.one,
+        //        "863b5c9a-7b83-4a88-b4d6-41a33bdba80e"
+        //        );
+        //else
+        //    FuseboxNode = box.gameObject;
 
 
+        handKeyAnimation = CreateNodeByNameAndId("vertx-event-handler", "");
+        handKeyAnimation.AddComponent<VertxEventHandler>();
+
+        //var guid = handKeyAnimation.GetComponent<NodeLink>().Guid;
+        //Debug.Log(guid);
+
+        //yield return new WaitForSeconds(5f);
+        ////DestroyObject(handKeyAnimation);
+
+        //GameObject switchOne = CreateNodeByNameAndId("arrow-switch-1", "36748b64-8251-4b76-8672-d67d5522dfb1");
+
+        //yield return new WaitForSeconds(5f);
+        ////DestroyObject(switchOne);
+
+        //GameObject switchTwo = CreateNodeByNameAndId("arrow-switch-2", "c74cf1eb-da54-448e-b5c2-a423d16064a4");
+
+
+
+        //yield return new WaitForSeconds(5f);
+        ////DestroyObject(switchTwo);
+
+        //GameObject switchThree = CreateNodeByNameAndId("arrow-switch-3", "ec53c75e-ec10-4014-8d96-f73f2e27ca82");
+
+
+        //yield return new WaitForSeconds(5f);
+        //DestroyObject(switchThree);
 
 
     }
 
+    // Method to create and return Vertex Node Link Game object 
+    private GameObject CreateNodeByNameAndId(string name, string id)
+    {
+        var vertxObject = SceneLink.Instance.transform.Find(name);
+        GameObject nodeGameObject;
+        if (vertxObject == null)
+        {
+            nodeGameObject = SceneLink.Instance.CreateNode(name, 
+                new Vector3(0f, 0f, 0f),
+                Quaternion.identity,
+                Vector3.one,
+                id
+           );
+        }
+        else
+        {
+            nodeGameObject = vertxObject.gameObject;
+        }
+        return nodeGameObject;
+
+    }
+
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+
+    }
 }
