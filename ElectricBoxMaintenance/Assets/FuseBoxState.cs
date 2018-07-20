ba6c375f7fe3e73904840ff0acc2f401e5a6f526
Sijan Shreesh Rana <Sijan_Rana@hotmail.com>
2018-07-20 10:43:29 +0100

    start

---
+++ ElectricBoxMaintenance/Assets/FuseBoxState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseBoxState : MonoBehaviour {
+
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StateUpdate(string state)
+    {
+        Debug.Log(state);
+        Destroy(GameObject.Instantiate(prefab, transform.position, transform.rotation), 5.0f);
+    }
+}
---
+++ ElectricBoxMaintenance/Assets/FuseBoxStateManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VertexUnityPlayer;
+
+[RequireComponent(typeof(SceneLink))]
+public class FuseBoxStateManager : MonoBehaviour {
+
+    public SceneLink sceneLink;
+
+    public GameObject fuseBoxNode;
+
+	// Use this for initialization
+	void Start () {
+        sceneLink = GetComponent<SceneLink>();
+        sceneLink.OnStateChange += SceneLink_OnStateChange;
+	}
+
+    private void SceneLink_OnStateChange(SceneLinkStatus oldState, SceneLinkStatus newState)
+    {
+        if(newState == SceneLinkStatus.Connected)
+        {
+            StartCoroutine(SetupFusebox());
+        }
+    }
+
+    IEnumerator SetupFusebox()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        var box = sceneLink.transform.Find("fusebox-twin");
+        if (box == null)
+            fuseBoxNode = sceneLink.CreateNode("fusebox-twin", Vector3.zero, Quaternion.identity, Vector3.one, null, null, "FuseBoxState");
+        else
+            fuseBoxNode = box.gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
