885418d803d385d2ee6142fa3cafee9679d08524
Sijan Shreesh Rana <Sijan_Rana@hotmail.com>
2018-07-31 10:01:17 +0100

    finish

--- ElectricBoxMaintenance/Assets/TheVertxEventHandler.cs
+++ ElectricBoxMaintenance/Assets/TheVertxEventHandler.cs
@@ -10,34 +10,25 @@ public class TheVertxEventHandler : MonoBehaviour {
 
     GameObject PreviousAnimationNode;
 
-    //DOOR_CLOSE ANIMATION : b4f004f9-4f04-4e74-9603-a0a695778f7d
-
-    int currentIndex;
-    bool isComplete;
-    int numberOfCompletedSteps;
-    bool flipSwitches;
-
-    int debugCounter = 0;
-    int switchReverseCounter = 0;
+    int currentStep;
 
     // Use this for initialization
     void Start () {
 
-        isComplete = false;
-        flipSwitches = false;
-
+        currentStep = 0;
         AnimationArray = new string[,,]
         {
             {
-                {"KEY_ANIMATION","62fe3789-6dc0-4be8-8de4-daf6be186bed", "0"},
-                {"DOOR_ANIMATION","fe72fbeb-7341-40ec-91a9-7d4387bf9ff6", "0"},
+                {"KEY_ANIMATION","62fe3789-6dc0-4be8-8de4-daf6be186bed", "1"},
+                {"DOOR_ANIMATION","fe72fbeb-7341-40ec-91a9-7d4387bf9ff6", "1"},
                 {"SWITCH_ONE","59d89c08-85a6-4e17-9edb-a2b648d0503e", "0" },
-                {"SWITCH_TWO","3fe496cd-7cf7-44d8-8388-74ac49e14986", "0"},
-                {"SWITCH_THREE","9d0db931-1698-47af-8080-3106cfede727", "0"},
-                //SAME SWITCHES USED
-                {"SWITCH_FOUR","a3418e0a-a9a3-4ee2-ada9-5eb0da5a0d29", "0"},
+                {"SWITCH_TWO","3fe496cd-7cf7-44d8-8388-74ac49e14986", "1"},
+                {"SWITCH_THREE","b9ed5bee-0f83-44ad-b9c8-943cfccfbbef", "0"},
+                {"SWITCH_FOUR","2fa79861-9ec8-423f-a113-e80a2c04739e", "1"},
                 {"SWITCH_FIVE","30fc82a8-7b3b-4cab-afb9-c8304a6c756d", "0"},
-                {"SWITCH_SIX","3930543b-229a-4f82-804a-90cd09eca5a5", "0"},
+                {"SWITCH_SIX","3930543b-229a-4f82-804a-90cd09eca5a5", "1"},
+                {"DOOR_FINISH","b4f004f9-4f04-4e74-9603-a0a695778f7d", "0"},
+                {"KEY_ANIMATION_FINISH","813d6861-2370-419d-951d-9fb71312c492","0"}
             }
         };
 
@@ -52,304 +43,79 @@ public class TheVertxEventHandler : MonoBehaviour {
             PreviousAnimationNode = SceneLink.Instance.transform.Find("KEY_ANIMATION").gameObject;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     public void OnUpdate(object message)
     {
-        debugCounter++;
-
-
-
-
-        //Destory previous animation
-        DestroyImmediate(PreviousAnimationNode);
-
         // Deserialize the message received by IOT
         Message _message = JsonConvert.DeserializeObject<Message>(message.ToString());
-
 
         string componentName = _message.name;
         string componentState = _message.state.ToString();
 
+        //Destory previous animation
+        DestroyImmediate(PreviousAnimationNode);
 
+        Debug.Log("Component name => " + componentName);
+        Debug.Log("Component state => " + componentState);
 
-
-        if (componentName == "SWITCH_ONE" && AnimationArray[0, 2, 2] == "1" && AnimationArray[0, 3, 2] == "1" && AnimationArray[0, 4, 2] == "1")
+        //SAME SWITCHES USED
+        if (currentStep > 4)
         {
-            AnimationArray[0, 2, 2] = "0";
-            AnimationArray[0, 3, 2] = "0";
-            AnimationArray[0, 4, 2] = "0";
-
-            flipSwitches = true;
-        }
-
-        if((componentName == "SWITCH_ONE" || componentName == "SWITCH_TWO" || componentName == "SWITCH_THREE") && flipSwitches)
-        {
-            if(AnimationArray[0, 2, 2] == "1" && AnimationArray[0, 3, 2] == "1" && AnimationArray[0, 4, 2] == "1")
+            switch (componentName)
             {
-                flipSwitches = false;
-            }
-            else
-            {
-                //switch (componentState)
-                //{
-                //    case "0":
-                //        componentState = "1";
-                //        break;
-                //    case "1":
-                //        componentState = "0";
-                //        break;
-                //}
+                case "SWITCH_THREE":
+                    componentName = "SWITCH_FOUR";
+                    break;
+                case "SWITCH_TWO":
+                    componentName = "SWITCH_FIVE";
+                    break;
+                case "SWITCH_ONE":
+                    componentName = "SWITCH_SIX";
+                    break;
+                case "DOOR_ANIMATION":
+                    componentName = "DOOR_FINISH";
+                    break;
+                case "KEY_ANIMATION":
+                    componentName = "KEY_ANIMATION_FINISH";
+                    break;
             }
         }
 
-
-
-
-
-        // Update array
-        UpdateComponentArray(componentState);
-
-        //if (componentName == "SWITCH_ONE" || componentName == "SWITCH_TWO" || componentName == "SWITCH_THREE"
-        //&& AnimationArray[0, 0, 0] == "1" && AnimationArray[0, 1, 0] == "1" && AnimationArray[0, 2, 0] == "1"
-        //&& AnimationArray[0, 3, 0] == "1" && AnimationArray[0, 4, 0] == "1")
-        //{
-        //    if (switchReverseCounter != 3)
-        //    {
-        //        switch (componentState)
-        //        {
-        //            case "0":
-        //                componentState = "1";
-        //                break;
-        //            case "1":
-        //                componentState = "0";
-        //                break;
-        //        }
-
-        //        switchReverseCounter++;
-        //    }
-        //    else
-        //    {
-        //        switchReverseCounter = 0;
-        //    }
-
-        //}
-
-
-        // index for which animation to play
-        currentIndex = FindNewIndex(componentName, componentState, "CurrentStep");
-
-        CheckLastStepCompleted(componentName, componentState);
-
-
-        if(!isComplete)
+        if(!isComplete(componentName, componentState))
         {
-
-            //Debug.Log("StartStep => " + currentIndex);
-
-            //print table before update
-            Debug.Log("<== BEFORE UPDATE ==>");
-            FindNewIndex(componentName, componentState, "Table");
-
-            //bring table after update
-            Debug.Log("<== AFTER UPDATE ==>");
-            FindNewIndex(componentName, componentState, "Table");
-
-            // Play animations
             ExecuteAnimation(componentName, componentState);
-            //Debug.Log("FinishStep => " + currentIndex);
         }
         else
         {
             PreviousAnimationNode = CreateNode("MAINTANENCE_MODEL", "2359e967-bcd4-44ab-b58f-6f7a8ca391e4");
         }
-
     }
 
- 
-    //void UpdateComponentArray(string _ComponentName, string _ComponentStatus)
-    //{
-    //    for (int i = 0; i < (AnimationArray.Length / 3) - 1; i++)
-    //    {
-    //        if (_ComponentName == AnimationArray[0, i, 0])
-    //        {
-    //            AnimationArray[0, i, 2] = _ComponentStatus;
-    //            break;
-    //        }
-    //    }
-    //}
-
-    //void ExecuteAnimation(string _ComponentName, string _ComponentStatus)
-    //{
-    //    for (int i = 0; i < (AnimationArray.Length / 3) - 1; i++)
-    //    {
-    //        if (AnimationArray[0,i,0] == _ComponentName)
-    //        {
-
-    //            if (AnimationArray[0, i, 2] == "0")
-    //            {
-    //                Debug.Log("DEBUG");
-    //                if(_ComponentName == "KEY_ANIMATION")
-    //                {
-    //                    Debug.Log("Trying to create "  + _ComponentName);
-    //                    CreateNode("KEY_ANIMATION", "62fe3789-6dc0-4be8-8de4-daf6be186bed");
-    //                }
-    //                else
-    //                {
-    //                    PreviousAnimationNode = CreateNode(AnimationArray[0, i, 0], AnimationArray[0, i, 1]);
-    //                    CurrentStep--;
-    //                }
-    //                break;
-    //            }
-    //            if(AnimationArray[0, i, 2] == "1")
-    //            {
-    //                PreviousAnimationNode = CreateNode(AnimationArray[0, i + 1, 0], AnimationArray[0, i + 1, 1]);
-    //                CurrentStep++;
-    //            }
-    //            break;
-    //        }
-    //    }
-    //}
-
-
-    void CheckLastStepCompleted(string _ComponentName, string _ComponentStatus)
+    //maintenence completed when all animations have been played
+    bool isComplete(string _componentName, string _componentState)
     {
-        int numberOfCompletedSteps = 0;
-
-        //last step has index of 7
-        if (currentIndex == 7 && AnimationArray[0, currentIndex, 2] == "1")
+        bool maintenenceComplete;
+        if(currentStep == 9)
         {
-            numberOfCompletedSteps = FindNewIndex(_ComponentName, _ComponentStatus, "State1");
+            maintenenceComplete = true;
         }
-
-        switch(numberOfCompletedSteps)
+        else
         {
-            case 7:
-                isComplete = true;
-                break;
-            default:
-                currentIndex = FindNewIndex(_ComponentName, _ComponentStatus, "CurrentStep");
-                break;
+            maintenenceComplete = false;
         }
+        return maintenenceComplete;
     }
 
-    void UpdateComponentArray(string _ComponentStatus)
-    {
-        //Debug.Log("CurrentStep => " + currentIndex);
-        AnimationArray[0, currentIndex, 2] = _ComponentStatus;
-    }
-
-
-    //decides what animation to run
     void ExecuteAnimation(string _ComponentName, string _ComponentStatus)
     {
-        //Debug.Log("CurrentStep => " + currentIndex);
-        //if key animation (DOOR ANIMATION) component is not called
-        if (_ComponentName != "DOOR_CLOSE")
+        if (_ComponentName == AnimationArray[0, currentStep, 0] && _ComponentStatus == AnimationArray[0, currentStep, 2])
         {
-            if(_ComponentStatus == "0")
-            {
-                currentIndex = FindNewIndex(_ComponentName, _ComponentStatus, "Name");
-            }
-
-            //switch (_ComponentStatus)
-            //{
-            //    //next animation depends on what component is switched to incorrect state
-            //    case "0":
-            //        currentIndex = FindNewIndex(_ComponentName, _ComponentStatus, "Name");
-            //        break;
-            //    //next animation depends on what status in our array is still incorrect
-            //    default:
-            //        break;
-            //}
-
-            //creates node for that index
-            CreateNode(AnimationArray[0, currentIndex, 0], AnimationArray[0, currentIndex, 1]);
-        }
-        //section for key animation (DOOR ANIMATION) component being changed 
-        else
-        {
-            //DOOR ANIMATION PLAYED ALWAYS IF CLOSED
-            if (_ComponentStatus == "0")
-            {
-                CreateNode("DOOR_OPEN", "b4f004f9-4f04-4e74-9603-a0a695778f7d");
-            }
-            else
-            {
-
-                CreateNode(AnimationArray[0, currentIndex, 0], AnimationArray[0, currentIndex, 1]);
-
-                //recalculate index value to start from where we left
-
-                //// currentIndex == STEP NUMBER if stepnumbe = 0 then outcome is step
-                //switch (currentIndex)
-                //{
-                //    case 0:
-                //        CreateNode(AnimationArray[0, currentIndex + 1, 0], AnimationArray[0, currentIndex + 1, 1]);
-                //        break;
-
-                //    default:
-                //        CreateNode(AnimationArray[0, currentIndex, 0], AnimationArray[0, currentIndex, 1]);
-                //        break;
-                //}
-                //currentIndex = FindNewIndex(_ComponentName, _ComponentStatus, "Status");
-            }
-
-        }
-
-    }
-
-    int FindNewIndex(string _ComponentName, string _ComponentStatus, string searchBy)
-    {
-        int index;
-
-        for (index = 0; index < (AnimationArray.Length / 3); index++)
-        {
-            switch (searchBy)
-            {
-                case "Name":
-                    if (AnimationArray[0, index, 0] == _ComponentName)
-                    {
-                        return index;
-                    }
-                    break;
-
-                case "CurrentStep":
-                    if (AnimationArray[0, index, 2] == "0")
-                    {
-                        return index;
-                    }
-                    break;
-                case "State1":
-                    if(AnimationArray[0, index, 2] == "1")
-                    {
-                        numberOfCompletedSteps++;
-                    }
-                    break;
-                case "Table":
-                    {
-                        Debug.Log(
-                            debugCounter + " " +
-                            AnimationArray[0, index, 0] + " " + 
-                            AnimationArray[0, index, 1] + " " +
-                            AnimationArray[0, index, 2]
-                            );
-                        break;
-                    }
-            }
-        }
-
-        if(searchBy == "State1")
-        {
-            return numberOfCompletedSteps;
+            CreateNode(AnimationArray[0, currentStep + 1, 0], AnimationArray[0, currentStep + 1, 1]);
+            currentStep++;
         }
         else
         {
-            return index;
+            CreateNode(AnimationArray[0, currentStep, 0], AnimationArray[0, currentStep, 1]);
         }
     }
 
